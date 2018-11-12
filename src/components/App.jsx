3ee716ebb25fe208c03c2f71f1625a307bbd8183
Huy Luu <lmhuy_lmhuy@yahoo.com>
2018-11-12 13:59:00 -0800

    Working on youtube Search API, still failing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,12 +13,18 @@ class App extends React.Component {
     });
   }
 
+  changeVideoList(list) {
+    this.setState({
+      allVideos: list
+    })
+  }
+
   render() {
     return (
       <div>
         <nav className="navbar">
           <div className="col-md-6 offset-md-3">
-            <div><h5><em>search</em> view goes here</h5></div>
+            <Search />
           </div>
         </nav>
         <div className="row">
--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,41 @@
-var searchYouTube = (options, callback) => {
-  // TODO
+var searchYouTube = (option, callback) => {
+  // TODO{query ='tennis', max = 5, key = window.YOUTUBE_API_KEY}, function(data) {console.log(data)}
+  var data = {
+    order: 'relevance',
+    q: 'tennis',
+    maxResults: 5,
+    videoEmbeddable: true,
+    key: window.YOUTUBE_API_KEY
+  }
+
+  $.ajax({
+        url: 'https://www.googleapis.com/youtube/v3/search',
+        type: 'GET',
+        data: $.extend({part: 'snippet'}, JSON.stringify(data)),
+        dataType: 'json',
+        success: function(data) {
+          //console.log(data);
+          callback();
+        },
+        error: function(error) {
+          console.log('Youtube: Failed to fetch messages', error);
+        }
+      });
+  // console.log('Hi');
 };
 
 window.searchYouTube = searchYouTube;
+
+
+// readAll: function(successCB, errorCB = null) {
+//   $.ajax({
+//     url: Parse.server,
+//     type: 'GET',
+//     data: { order: '-createdAt' },
+//     contentType: 'application/json',
+//     success: successCB,
+//     error: errorCB || function(error) {
+//       console.error('chatterbox: Failed to fetch messages', error);
+//     }
+//   });
+// },
