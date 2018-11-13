6715b67a39665702cca3847df65fa39e7e5ac6cc
Chen Wilson <chenlwilson@gmail.com>
2018-11-12 16:38:14 -0800

    Youtube API call working

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,22 +1,10 @@
 var Search = () => (
-
-  // var options = {
-  //   query:$('.form-control').val(),
-  //   max: 5,
-  //   //key: window.YOUTUBE_API_KEY
-  // }
-
-  // var callBack = function(data) {
-  //     console.log(data);
-  //     App.changeVideoList(data);
-  // }
-
     <div>
     <h5>
       <em>
         <div className="search-bar form-inline">
             <input className="form-control" type="text"/>
-            <button className="btn hidden-sm-down" onClick={()=>{searchYouTube({query:'tennis', max: 5, key: window.YOUTUBE_API_KEY}, function() {console.log('success')})}}>
+            <button className="btn hidden-sm-down" onClick={()=>{searchYouTube({query:'tennis', max: 5, key: window.YOUTUBE_API_KEY}, function(item) {console.log(item)})}}>
               <span className="glyphicon glyphicon-search"></span>
             </button>
          </div>
--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,40 +1,25 @@
-var searchYouTube = (option, callback) => {
-  // input: {query ='tennis', max = 5, key = window.YOUTUBE_API_KEY}, function(data) {console.log(data)}
+var searchYouTube = (options, callback) => {
+  // input:
+  //{query ='tennis', max = 5, key = window.YOUTUBE_API_KEY}
+  //function(data) {console.log(data)}
 
-  $.ajax({
-        url: 'https://www.googleapis.com/youtube/v3/search',
-        type: 'GET',
-        data: {
-          part: 'snippet',
-          order: 'relevance',
-          q: 'tennis',
-          maxResults: 5,
-          videoEmbeddable: true,
-          key: window.YOUTUBE_API_KEY
-        },
-        dataType: 'json',
-        success: function(data) {
-          console.log(data);
-          callback();
-        },
-        error: function(error) {
-          console.log('Youtube: Failed to fetch messages', error);
-        }
+  $.get('https://www.googleapis.com/youtube/v3/search', {
+        part: 'snippet',
+        order: 'relevance',
+        q: options.query,
+        maxResults: options.max,
+        key: options.key,
+        videoEmbeddable: true,
+        type: 'video'
+      })
+      .done(function(data) {
+        console.log('success');
+        console.log(data);
+        callback(data.items);
+      })
+      .fail(function() {
+        console.log('Youtube: Failed to fetch messages');
       });
 };
 
 window.searchYouTube = searchYouTube;
-
-
-// readAll: function(successCB, errorCB = null) {
-//   $.ajax({
-//     url: Parse.server,
-//     type: 'GET',
-//     data: { order: '-createdAt' },
-//     contentType: 'application/json',
-//     success: successCB,
-//     error: errorCB || function(error) {
-//       console.error('chatterbox: Failed to fetch messages', error);
-//     }
-//   });
-// },
