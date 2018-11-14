ede1a7e1f9d457adba76e45ac1676473ec5c1a3b
Chen Wilson <chenlwilson@gmail.com>
2018-11-13 19:29:42 -0800

    Complete connecting app to the YouTube API

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,16 +1,17 @@
+//Complete connecting app to the YouTube API
 var Search = (props) => (
   <div>
     <h5>
       <em>
         <div className="search-bar form-inline">
-            <input className="form-control" type="text"/>
-            <button className="btn hidden-sm-down" onClick={()=>{props.searchVideo($('.form-control').val());}}>
-              <span className="glyphicon glyphicon-search"></span>
-            </button>
-         </div>
-        </em>
-      </h5>
-    </div>
+          <input className="form-control" type="text"/>
+          <button className="btn hidden-sm-down" onClick={()=>{ props.searchVideo($('.form-control').val()); }}>
+            <span className="glyphicon glyphicon-search"></span>
+          </button>
+        </div>
+      </em>
+    </h5>
+  </div>
 );
 
 window.Search = Search;
